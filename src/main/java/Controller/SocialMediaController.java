4f6a778b1ce57c94999729da8f9cd5931920ff28
Christopher Magtibay <cagmagtibay@live.com>
2023-04-03 22:58:40 +0000

    Changed: Controller;AccountDAO

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -17,6 +17,14 @@ import Service.MessageService;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accountService;
+    MessageService messageService;
+
+    public SocialMediaController() {
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
+    
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -46,17 +54,39 @@ public class SocialMediaController {
 
     /**
      * Requirement 1 Handler: Process new User registrations
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. 
+     * The body will contain a representation of a JSON Account, but will not contain an account_id.
+     *
+     * - The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, and an Account with that username does not already exist. 
+     *   If all these conditions are met, the response body should contain a JSON of the Account, including its account_id. 
+     *   The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+     * - If the registration is not successful, the response status should be 400. (Client error)
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
     private void postNewUserHandler(Context context) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Account user = mapper.readValue(context.body(), Account.class);
-        
+        // Author addedUser = accountService.addUser(user);
+
     }
 
     /**
      * Requirement 2 Handler: Process User logins
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. 
+     * The request body will contain a JSON representation of an Account, not containing an account_id. 
+     * In the future, this action may generate a Session token to allow the user to securely use the site. We will not worry about this for now.
+     *
+     * - The login will be successful if and only if the username and password provided in the request body JSON match a real account existing on the database. 
+     *   If successful, the response body should contain a JSON of the account in the response body, including its account_id. 
+     *   The response status should be 200 OK, which is the default.
+     * - If the login is not successful, the response status should be 401. (Unauthorized)
+     *
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
@@ -66,6 +96,17 @@ public class SocialMediaController {
 
     /**
      * Requirement 3 Handler: Process new messages
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to submit a new post on the endpoint POST localhost:8080/messages. 
+     * The request body will contain a JSON representation of a message, which should be persisted to the database, but will not contain a message_id.
+     * 
+     * - The creation of the message will be successful if and only if the message_text is not blank, is under 255 characters, and posted_by refers to a real, existing user. 
+     *   If successful, the response body should contain a JSON of the message, including its message_id. 
+     *   The response status should be 200, which is the default. 
+     *   The new message should be persisted to the database.
+     * - If the creation of the message is not successful, the response status should be 400. (Client error)
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
@@ -75,6 +116,14 @@ public class SocialMediaController {
 
     /**
      * Requirement 4 Handler: Retrieve all messages
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages.
+     * 
+     * - The response body should contain a JSON representation of a list containing all messages retrieved from the database. 
+     *   It is expected for the list to simply be empty if there are no messages. 
+     *   The response status should always be 200, which is the default.
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
@@ -84,6 +133,14 @@ public class SocialMediaController {
 
     /**
      * Requirement 5 Handler: Retrieve a message by its ID
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{message_id}.
+     * 
+     * - The response body should contain a JSON representation of the message identified by the message_id. 
+     *   It is expected for the response body to simply be empty if there is no such message. 
+     *   The response status should always be 200, which is the default.
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
@@ -93,6 +150,16 @@ public class SocialMediaController {
 
     /**
      * Requirement 6 Handler: Delete a message identified by its ID
+     * 
+     * TO-DO from Requirements:
+     * As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{message_id}.
+     * 
+     * - The deletion of an existing message should remove an existing message from the database. 
+     *   If the message existed, the response body should contain the now-deleted message. 
+     *   The response status should be 200, which is the default.
+     * - If the message did not exist, the response status should be 200, but the response body should be empty. 
+     *   This is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond with the same type of response.
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
@@ -102,6 +169,17 @@ public class SocialMediaController {
     
     /**
      * Requirement 7 Handler: Update a message text identified by its ID
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to submit a PATCH request on the endpoint PATCH localhost:8080/messages/{message_id}. 
+     * The request body should contain a new message_text values to replace the message identified by message_id. 
+     * The request body can not be guaranteed to contain any other information.
+     * 
+     * - The update of a message should be successful if and only if the message id already exists and the new message_text is not blank and is not over 255 characters. 
+     *   If the update is successful, the response body should contain the full updated message (including message_id, posted_by, message_text, and time_posted_epoch), and the response status should be 200, which is the default. 
+     *   The message existing on the database should have the updated message_text.
+     * - If the update of the message is not successful for any reason, the response status should be 400. (Client error)
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
@@ -111,6 +189,14 @@ public class SocialMediaController {
 
     /**
      * Requirement 8 Handler: Retrieve all messages written by a particular user
+     * 
+     * TO-DO from Requirements:
+     * As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{account_id}/messages.
+     * 
+     * - The response body should contain a JSON representation of a list containing all messages posted by a particular user, which is retrieved from the database. 
+     *   It is expected for the list to simply be empty if there are no messages. 
+     *   The response status should always be 200, which is the default.
+     * 
      * @param context
      * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object
      */
--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -3,6 +3,43 @@ package DAO;
 import Model.Account;
 import Util.ConnectionUtil;
 
+import java.sql.*;
+
 public class AccountDAO {
     
+    /**
+     * TO-DO: Process new User registration
+     */
+    public Account insertAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            if (account.getUsername() == null) {                                                // check if username is blank
+                throw new SQLException("Username must not be blank.");                   // throw exception and return null if so
+            }
+            else if (account.getPassword().length() < 4) {                                      // check if password is at least 4 characters long
+                throw new SQLException("Password must be at least 4 characters long.");  // throw exception and return null if not
+            }
+
+            // check if username doesn't already exist
+            String databaseCheck = "SELECT * from account WHERE username = (?);";
+            PreparedStatement checkStatement = connection.prepareStatement(databaseCheck);
+            checkStatement.setString(1, account.getUsername());
+            ResultSet checkResultSet = checkStatement.executeQuery();
+            if (checkResultSet.next()) {
+                throw new SQLException("Username already exists.");
+            }
+
+            // All input checks pass -> insert account into table
+
+
+
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * TO-DO: Process User login
+     */
 }
